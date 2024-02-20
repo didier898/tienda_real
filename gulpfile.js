a80e202dec14c55434e86631e8b8dd24ac9c5632
Didier <didier002010@gmail.com>
2024-02-20 14:37:44 -0500

    commit zegundo

--- gulpfile.js
+++ gulpfile.js
@@ -7,55 +7,58 @@ const autoprefixer = require('autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const cssnano = require('cssnano');
 
-// Imagenes
+// Imágenes
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
-function css( done ) {
-    src('src/scss/app.scss')
-        .pipe( sourcemaps.init() )
-        .pipe( sass() )
-        .pipe( postcss([ autoprefixer(), cssnano() ]) )
-        .pipe( sourcemaps.write('.'))
-        .pipe( dest('build/css') )
-
-    done();
+// Compilación de CSS
+function css() {
+    return src('src/scss/app.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/css'));
 }
 
+// Optimización de imágenes
 function imagenes() {
     return src('src/img/**/*')
-        .pipe( imagemin({ optimizationLevel: 3 }) )
-        .pipe( dest('build/img') )
+        .pipe(imagemin({ optimizationLevel: 3 }))
+        .pipe(dest('build/img'));
 }
 
+// Conversión a formato WebP
 function versionWebp() {
     const opciones = {
         quality: 50
-    }
+    };
     return src('src/img/**/*.{png,jpg}')
-        .pipe( webp( opciones ) )
-        .pipe( dest('build/img') )
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
 }
+
+// Conversión a formato AVIF
 function versionAvif() {
     const opciones = {
         quality: 50
-    }
+    };
     return src('src/img/**/*.{png,jpg}')
-        .pipe( avif( opciones ) )
-        .pipe( dest('build/img'))
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'));
 }
 
+// Tarea de desarrollo
 function dev() {
-    watch( 'src/scss/**/*.scss', css );
-    watch( 'src/img/**/*', imagenes );
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);
 }
 
+// Tarea de construcción
+const build = series(imagenes, versionWebp, versionAvif, css);
 
-exports.css = css;
+// Exporta las tareas
+exports.build = build;
 exports.dev = dev;
-exports.imagenes = imagenes;
-exports.versionWebp = versionWebp;
-exports.versionAvif = versionAvif;
-exports.default = series( imagenes, versionWebp, versionAvif, css, dev  );
-/* mfmfm*/
+exports.default = series(build, dev);
